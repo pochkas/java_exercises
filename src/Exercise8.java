fb18944a01e75ae071990ebc45f9baad76058bdf
Svetlana <lavsvetlav@gmail.com>
2022-12-29 10:44:33 +0300

    initial commit

---
+++ src/Exercise8.java
@@ -0,0 +1,14 @@
+public class Exercise8 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(
+                """
+                                   J    a   v     v  a                                                 \s
+                                   J   a a   v   v  a a                                                \s
+                                J  J  aaaaa   V V  aaaaa                                               \s
+                                 JJ  a     a   V  a     a
+                        """);
+    }
+}
