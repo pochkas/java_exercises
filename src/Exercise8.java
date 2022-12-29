e25763f01b38cf06e349c385a5a5b01fde5a9125
Svetlana <lavsvetlav@gmail.com>
2022-12-29 10:47:23 +0300

    second commit

--- src/Exercise8.java
+++ src/Exercise8.java
@@ -3,12 +3,11 @@ public class Exercise8 {
     public static void main(String[] args) {
 
 
-        System.out.println(
-                """
-                                   J    a   v     v  a                                                 \s
-                                   J   a a   v   v  a a                                                \s
-                                J  J  aaaaa   V V  aaaaa                                               \s
-                                 JJ  a     a   V  a     a
-                        """);
+        System.out.println("""
+                           J    a   v     v  a                                                 \s
+                           J   a a   v   v  a a                                                \s
+                        J  J  aaaaa   V V  aaaaa                                               \s
+                         JJ  a     a   V  a     a
+                """);
     }
 }
